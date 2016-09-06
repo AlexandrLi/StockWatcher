502df60dbf5463ce0074fc08183cb94d35e118b5
Alexandr_Li <muzikant1990@gmail.com>
2016-09-06 11:52:20 +0600

    Refactor using UiBinder

--- src/com/epam/ali/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/epam/ali/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -2,65 +2,22 @@ package com.epam.ali.gwt.sample.stockwatcher.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.*;
-import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.*;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class StockWatcher implements EntryPoint {
 
     private static final int REFRESH_INTERVAL = 3000;
-    private VerticalPanel mainPanel = new VerticalPanel();
-    private FlexTable stocksFlexTable = new FlexTable();
-    private HorizontalPanel addPanel = new HorizontalPanel();
-    private TextBox newSymbolTextBox = new TextBox();
-    private Button addStockButton;
-    private Label lastUpdateLabel = new Label();
-    private Label errorMsgLabel = new Label();
-    private List<String> stocks = new ArrayList<String>();
     private StockWatcherConstants constants = GWT.create(StockWatcherConstants.class);
-    private StockWatcherMessages messages = GWT.create(StockWatcherMessages.class);
     private StockPriceServiceAsync stockPriceService = GWT.create(StockPriceService.class);
+    private StockWatcherUI stockWatcher = new StockWatcherUI();
 
 
     public void onModuleLoad() {
         Window.setTitle(constants.stockWatcher());
-        RootPanel.get("appTitle").add(new Label(constants.stockWatcher()));
-
-        stocksFlexTable.setText(0, 0, constants.symbol());
-        stocksFlexTable.setText(0, 1, constants.price());
-        stocksFlexTable.setText(0, 2, constants.change());
-        stocksFlexTable.setText(0, 3, constants.remove());
-
-        stocksFlexTable.setCellPadding(6);
-        stocksFlexTable.getRowFormatter().addStyleName(0, "watchListHeader");
-        stocksFlexTable.addStyleName("watchList");
-        stocksFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
-        stocksFlexTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
-        stocksFlexTable.getCellFormatter().addStyleName(0, 3, "watchListRemoveColumn");
-
-        addStockButton = new Button(constants.add());
-        addPanel.add(newSymbolTextBox);
-        addPanel.add(addStockButton);
-        addPanel.addStyleName("addPanel");
-
-        errorMsgLabel.setStyleName("errorMessage");
-        errorMsgLabel.setVisible(false);
-
-        mainPanel.add(errorMsgLabel);
-        mainPanel.add(stocksFlexTable);
-        mainPanel.add(addPanel);
-        mainPanel.add(lastUpdateLabel);
-
-        RootPanel.get("stockList").add(mainPanel);
-
-        newSymbolTextBox.setFocus(true);
+        RootPanel.get("stockList").add(stockWatcher);
 
         Timer refreshTimer = new Timer() {
             @Override
@@ -70,57 +27,6 @@ public class StockWatcher implements EntryPoint {
         };
         refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
 
-        addStockButton.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                addStock();
-            }
-        });
-
-        newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
-            @Override
-            public void onKeyDown(KeyDownEvent event) {
-                if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    addStock();
-                }
-            }
-        });
-    }
-
-    private void addStock() {
-        final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
-        newSymbolTextBox.setFocus(true);
-
-        if (!symbol.matches("[0-9A-Z\\\\.]{1,10}$")) {
-            Window.alert(messages.invalidSymbol(symbol));
-            newSymbolTextBox.selectAll();
-            return;
-        }
-        if (stocks.contains(symbol)) {
-            Window.alert(messages.alreadyExists(symbol));
-            newSymbolTextBox.selectAll();
-            return;
-        }
-        int row = stocksFlexTable.getRowCount();
-        stocks.add(symbol);
-        stocksFlexTable.setText(row, 0, symbol);
-        stocksFlexTable.setWidget(row, 2, new Label());
-        stocksFlexTable.getCellFormatter().addStyleName(row, 1, "watchListNumericColumn");
-        stocksFlexTable.getCellFormatter().addStyleName(row, 2, "watchListNumericColumn");
-        stocksFlexTable.getCellFormatter().addStyleName(row, 3, "watchListRemoveColumn");
-        newSymbolTextBox.setText("");
-
-        Button removeStockButton = new Button("x", new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                int removedIndex = stocks.indexOf(symbol);
-                stocks.remove(removedIndex);
-                stocksFlexTable.removeRow(removedIndex + 1);
-            }
-        });
-        removeStockButton.addStyleDependentName("remove");
-        stocksFlexTable.setWidget(row, 3, removeStockButton);
-        refreshWatchList();
     }
 
     private void refreshWatchList() {
@@ -134,45 +40,16 @@ public class StockWatcher implements EntryPoint {
                 if (caught instanceof DelistedException) {
                     details = "Company '" + ((DelistedException) caught).getSymbol() + "' was delisted";
                 }
-                errorMsgLabel.setText(details);
-                errorMsgLabel.setVisible(true);
+                stockWatcher.setErrorMsg(details);
             }
 
             @Override
             public void onSuccess(StockPrice[] result) {
-                updateTable(result);
+                stockWatcher.updateTable(result);
             }
         };
-        stockPriceService.getPrices(stocks.toArray(new String[0]), callback);
+        stockPriceService.getPrices(stockWatcher.getStocks().toArray(new String[0]), callback);
     }
 
-    private void updateTable(StockPrice[] prices) {
-        for (StockPrice price : prices) {
-            updateTable(price);
-        }
-        lastUpdateLabel.setText(messages.lastUpdate(new Date()));
-        errorMsgLabel.setVisible(false);
-    }
 
-    private void updateTable(StockPrice price) {
-        if (!stocks.contains(price.getSymbol())) {
-            return;
-        }
-        int row = stocks.indexOf(price.getSymbol()) + 1;
-        String priceText = NumberFormat.getFormat("#,##0.00").format(price.getPrice());
-        NumberFormat changeFormat = NumberFormat.getFormat("+#,##0.00;-#,##0.00");
-        String changeText = changeFormat.format(price.getChange());
-        String changePercentText = changeFormat.format(price.getChangePercent());
-
-        stocksFlexTable.setText(row, 1, priceText);
-        Label changeWidget = (Label) stocksFlexTable.getWidget(row, 2);
-        changeWidget.setText(changeText + " (" + changePercentText + "%)");
-        String changeStyleName = "noChange";
-        if (price.getChangePercent() < -0.1) {
-            changeStyleName = "negativeChange";
-        } else if (price.getChangePercent() > 0.1) {
-            changeStyleName = "positiveChange";
-        }
-        changeWidget.setStyleName(changeStyleName);
-    }
 }
